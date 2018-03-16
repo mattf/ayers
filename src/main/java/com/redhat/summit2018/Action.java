d4d4fa27abb8acf87d818727cb41b734066397ce
Matthew Farrellee <matt@redhat.com>
2018-03-16 09:19:27 -0400

    add example input spec

--- src/main/java/com/redhat/summit2018/Action.java
+++ src/main/java/com/redhat/summit2018/Action.java
@@ -7,6 +7,14 @@ import com.google.gson.JsonObject;
  */
 public class Action {
    public static JsonObject main(JsonObject args) {
+      // Example args["swiftObj"] -
+      // "swiftObj": {
+      //    "container": "ayers-images",
+      //    "method": "PUT",
+      //    "object": "zero.jpg",
+      //    "token": "AUTH_xyz...",
+      //    "url": "http://host:port/v1/AUTH_gv0"
+      // }
       return args;
    }
 }
