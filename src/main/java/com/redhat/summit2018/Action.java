655fb8e8ad5c6da30e5a65790f752a91ab8020e2
Matthew Farrellee <matt@redhat.com>
2018-03-19 16:12:10 -0400

    factor image getter

--- src/main/java/com/redhat/summit2018/Action.java
+++ src/main/java/com/redhat/summit2018/Action.java
@@ -27,40 +27,15 @@ public class Action
    private static final Logger LOGGER = LogManager.getLogger(Action.class);
 
    public static JsonObject main(JsonObject args) {
-      // Example args["swiftObj"] -
-      // "swiftObj": {
-      //    "container": "ayers-images",
-      //    "method": "PUT",
-      //    "object": "zero.jpg",
-      //    "token": "AUTH_xyz...",
-      //    "url": "http://host:port/v1/AUTH_gv0"
-      // }
-
       // YOLOv2 service
       // Input: { "image": base64(img) } -- Image
       // Output: [ { "score": float, "voc": "[category]" }, ... ] -- Label[]
-
-      Image image = new Image();
-      if (args.has("imageFile")) {
-         String filename = args.get("imageFile").getAsString();
-
-         File file = new File(filename);
-         try (FileInputStream in = new FileInputStream(file)) {
-            byte data[] = new byte[(int) file.length()];
-            in.read(data); // TODO: check result is -1
-            image.setImage(Base64.getEncoder().encodeToString(data));
-         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-         } catch (IOException e) {
-            e.printStackTrace();
-         }
-      }
 
       ResteasyClient client = new ResteasyClientBuilder().build();
       Response response = client.target(getModelEndpoint(args))
          .request()
          .accept(MediaType.APPLICATION_JSON_TYPE)
-         .post(Entity.entity(image, MediaType.APPLICATION_JSON_TYPE));
+         .post(Entity.entity(getImage(args), MediaType.APPLICATION_JSON_TYPE));
       int status = response.getStatus();
       LOGGER.info("status: " + status);
       Label[] labels = response.readEntity(Label[].class);
@@ -80,5 +55,35 @@ public class Action
          uri = "http://localhost:8080/v2/yolo";
       }
       return uri;
+   }
+
+
+   private static Image getImage(JsonObject args) {
+      // Example args["swiftObj"] -
+      // "swiftObj": {
+      //    "container": "ayers-images",
+      //    "method": "PUT",
+      //    "object": "zero.jpg",
+      //    "token": "AUTH_xyz...",
+      //    "url": "http://host:port/v1/AUTH_gv0"
+      // }
+
+      Image image = new Image();
+      if (args.has("imageFile")) {
+         String filename = args.get("imageFile").getAsString();
+
+         File file = new File(filename);
+         try (FileInputStream in = new FileInputStream(file)) {
+            byte data[] = new byte[(int) file.length()];
+            in.read(data); // TODO: check result is -1
+            image.setImage(Base64.getEncoder().encodeToString(data));
+         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+
+      return image;
    }
 }
